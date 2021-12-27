23aa8bbe92a75c665e2e6c27324094412a3e9141
amelia.risner0@gmail.com <arisner@berkeley.edu>
2021-12-26 18:09:05 -0800

    assignment completed

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your solution in this file!
+const employee = {
+    name: "Rafadas",
+    streetAddress: "Discord"
+  };
+
+ function updateEmployeeWithKeyAndValue(object, key, value) {
+   const newEmployee = {...object};
+   newEmployee[key] = value;
+   return newEmployee;
+ }
+
+ function destructivelyUpdateEmployeeWithKeyAndValue(object, key, value) {
+   object[key] = value;
+   return object;
+ }
+
+ function deleteFromEmployeeByKey(object, key) {
+   const newEmployee = {...object};
+   delete newEmployee[key];
+   return newEmployee;
+ }
+
+ function destructivelyDeleteFromEmployeeByKey(object, key) {
+   delete object[key];
+   return object;
+ }
